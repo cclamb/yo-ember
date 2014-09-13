738ab36b3c41920ed5067816641f8797fdcaa89c
cclamb <chrislambistan@gmail.com>
2014-09-13 09:06:04 -0600

    first import

---
+++ app/scripts/app.js
@@ -0,0 +1,10 @@
+var YoEmber = window.YoEmber = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/components/*');
+require('scripts/views/*');
+require('scripts/router');
